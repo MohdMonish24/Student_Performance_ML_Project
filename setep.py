02498307ab4b5d2269d5b6ea35189675b6d69dbd
Mohd Monish <monishkhank1998@gmail.com>
2023-06-28 08:54:56 +0530

    logging and exception

--- setep.py
+++ setep.py
@@ -3,7 +3,7 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-    '''
+    '''git
     this function will return the list of requirements
     '''
     requirements=[]
